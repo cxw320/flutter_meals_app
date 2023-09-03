203340aeab4e612aeb20ea39788c55bef9b11e85
Caroline Wang <cxw320@case.edu>
2023-09-03 18:53:15 -0400

    add meals screen and meal item widget

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/models/category.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context, Category category) {
+    //this filters the dummyMeals list to only meals that have categories that match the category id
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
     // load diff screen
     Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (ctx) => const MealsScreen(
-          title: "test",
-          meals: [],
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
         ),
       ),
     );
@@ -36,7 +42,12 @@ class CategoriesScreen extends StatelessWidget {
             // availableCategories.map((category) => CategoryGridItem(category: category)).toList()
             [
           for (final category in availableCategories)
-            CategoryGridItem(category: category)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
+            )
         ],
       ),
     );
